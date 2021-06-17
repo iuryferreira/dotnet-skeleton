c98a0cee549b94d3a5bf41984b95f6976297157a
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-06-17 18:48:37 -0300

    🔧 rename param field

--- run.ps1
+++ run.ps1
@@ -1,5 +1,5 @@
 $command=$args[0]
-$command2=$args[1]
+$param=$args[1]
 
 $solutionName = (Get-Item "*.sln").BaseName
 
@@ -7,12 +7,12 @@ switch ($command)
 {
     "sln:name" { 
         $old_name = (Get-Item "*.sln").BaseName
-        if($old_name -ne $command2){
-            Get-ChildItem -Recurse -Filter "$old_name.*" | Rename-Item -NewName {$_.name -Replace "$old_name","$command2" }
-            (Get-Content -path "$command2.sln" -Raw) -replace "$old_name\.","$command2." | Set-Content -Path "$command2.sln"
+        if($old_name -ne $param){
+            Get-ChildItem -Recurse -Filter "$old_name.*" | Rename-Item -NewName {$_.name -Replace "$old_name","$param" }
+            (Get-Content -path "$param.sln" -Raw) -replace "$old_name\.","$param." | Set-Content -Path "$param.sln"
         }
      }
-    "sln:add" { dotnet sln add "src/$solutionName.$command2" }
-    "sln:remove" { dotnet sln remove "src/$solutionName.$command2" }
+    "sln:add" { dotnet sln add "src/$solutionName.$param" }
+    "sln:remove" { dotnet sln remove "src/$solutionName.$param" }
     default { "Comando não encontrado."}
 }
